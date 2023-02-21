ae84d8d259393cd95101cf4ed4883585a765b05c
Ashish Shubham <ashubham@gmail.com>
2023-02-21 15:53:16 -0800

    Data on getData click

--- packages/sheets/src/ui/components/search-bar/search-bar.tsx
+++ packages/sheets/src/ui/components/search-bar/search-bar.tsx
@@ -1,24 +1,48 @@
-import { SearchBarEmbed } from '@thoughtspot/visual-embed-sdk/react';
+import {
+  SearchBarEmbed,
+  useEmbedRef,
+} from '@thoughtspot/visual-embed-sdk/react';
+import { HostEvent } from '@thoughtspot/visual-embed-sdk';
+import { useEffect } from 'preact/hooks';
 import { useLoader } from 'widgets/lib/loader';
 import { Vertical } from 'widgets/lib/layout/flex-layout';
 import { useShellContext } from 'gsuite-shell';
-import { parseHeaderAndRows } from './search-bar.util';
+import { parseHeaderAndRows, debounce } from './search-bar.util';
+import { getQueryResult } from '../../services/api';
 import './search-bar.scss';
 
 export const TSSearchBar = () => {
   const loader = useLoader();
   const { run } = useShellContext();
+  const embed = useEmbedRef();
 
   const onData = (event) => {
     const payload = event.data.embedAnswerData;
     const { colNames, rows } = parseHeaderAndRows(payload);
     run('updateData', colNames, rows);
   };
+  const onGetDataClick = debounce(() => {
+    embed.current.trigger(HostEvent.GetTML).then(async (data) => {
+      const query = data.answer.search_query;
+      if (!query) {
+        return;
+      }
+      const source = data.answer.tables[0].id;
+      const { colNames, rows } = await getQueryResult(query, source);
+      loader.show();
+      await run('updateData', colNames, rows);
+      loader.hide();
+    });
+  }, 100);
   return (
     <Vertical className="search-bar">
       <SearchBarEmbed
-        onData={onData}
+        ref={embed}
         onLoad={() => loader.hide()}
+        onGetDataClick={() => {
+          loader.show();
+          return onGetDataClick();
+        }}
       ></SearchBarEmbed>
     </Vertical>
   );
