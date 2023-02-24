3a197ec3a38919916ea92b8d9b85ebe4cf84b216
nirmay-patel <nirmay.patel@thoughtspot.com>
2023-02-25 00:11:33 +0530

    Change version of modules and setup slides folder

--- packages/slides/src/ui/components/home/home.tsx
+++ packages/slides/src/ui/components/home/home.tsx
@@ -1,5 +1,6 @@
 import { useLoader } from 'widgets/lib/loader';
 import { Vertical } from 'widgets/lib/layout/flex-layout';
+import { useTranslations } from 'i18n';
 import { Button } from 'widgets/lib/button';
 import { useShellContext } from 'gsuite-shell';
 import './home.scss';
@@ -8,12 +9,13 @@ import React from 'react';
 export const Home = () => {
   const loader = useLoader();
   const { run } = useShellContext();
+  const { t } = useTranslations();
 
   return (
     <Vertical className="home">
       <Button
         onClick={() => console.log('Clicks clicked')}
-        text={'Browse Content'}
+        text={t.BROWSE_TS}
         type={'PRIMARY'}
       ></Button>
     </Vertical>
