0b154314e1cec99414b30b69686117c5e9037228
adityamittal3107 <aditya.mittal@thoughtspot.com>
2024-10-07 08:05:43 +0530

    Fix LB styling in release

--- packages/slides/src/ui/components/liveboard/liveboard.tsx
+++ packages/slides/src/ui/components/liveboard/liveboard.tsx
@@ -179,6 +179,10 @@ const PrerenderedLiveboardShell = () => {
                 width: '100%',
                 height: '100%',
               },
+              '.embed-module__tsEmbedContainer': {
+                width: '100%',
+                height: '100%',
+              },
             },
           },
         },
