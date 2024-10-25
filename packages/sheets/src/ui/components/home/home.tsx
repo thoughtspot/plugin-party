a529c56922db590de6d3c4087d83f99d79a557e7
Aditya Mittal <114516106+adityamittal3107@users.noreply.github.com>
2024-10-25 14:08:10 +0530

    SCAL-190687 Add errror handling for privileges (#98)

--- packages/sheets/src/ui/components/home/home.tsx
+++ packages/sheets/src/ui/components/home/home.tsx
@@ -8,6 +8,7 @@ import { useTranslations } from 'i18n';
 import { useLoader } from 'widgets/lib/loader';
 import { useShellContext } from 'gsuite-shell';
 import { SuccessBanner } from 'widgets/lib/success-banner';
+import { getSessionInfo } from '@thoughtspot/visual-embed-sdk';
 import styles from './home.module.scss';
 import { Routes } from '../../routes';
 import { getToken } from '../../services/api';
@@ -16,18 +17,6 @@ export const Home = () => {
   const { t } = useTranslations();
   const loader = useLoader();
   const { run } = useShellContext();
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const token = await getToken();
-        run('setToken', token.token, token.timeToLive);
-      } finally {
-        loader.hide();
-      }
-    };
-
-    fetchToken();
-  }, []);
 
   const [errorMessage, setErrorMessage] = useState({
     visible: false,
@@ -35,6 +24,45 @@ export const Home = () => {
   });
   const [selectedRefresh, setSelectedRefresh] = useState(t.REFRESH_ALL_SHEETS);
   const [success, setSuccess] = useState(false);
+  const [isPrivileged, setIsPrivileged] = useState(false);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const userInfo = await getSessionInfo();
+      const isUserPrivileged =
+        userInfo?.privileges?.includes('DATADOWNLOADING') ||
+        userInfo?.privileges?.includes('ADMINISTRATION');
+      setIsPrivileged(isUserPrivileged);
+      if (!isUserPrivileged) {
+        setErrorMessage({
+          visible: true,
+          message: t.PRIVILEGE_REQUIRED,
+        });
+        loader.hide();
+      }
+    };
+    getUserInfo();
+  }, []);
+
+  useEffect(() => {
+    if (isPrivileged) {
+      getToken().then((token) => {
+        if (token.token) {
+          run('setToken', token.token, token.timeToLive);
+          setErrorMessage({
+            visible: false,
+            message: '',
+          });
+        } else {
+          setErrorMessage({
+            visible: true,
+            message: t.TOKEN_FETCH_FAILED,
+          });
+        }
+        loader.hide();
+      });
+    }
+  }, [isPrivileged]);
 
   const handleSelectionChange = useCallback(
     (value: string) => {
@@ -76,27 +104,31 @@ export const Home = () => {
           setErrorMessage({ ...errorMessage, visible: false })
         }
       />
-      <Card
-        id={0}
-        title={t.INSERT_TS_DATA}
-        subTitle={t.INSERT_TS_DATA_DESCRIPTION}
-        firstButton={t.SELECT_TS_DATA}
-        firstButtonType={'PRIMARY'}
-        onFirstButtonClick={() => route(Routes.SEARCHBAR)}
-      />
-      <Card
-        id={1}
-        title={t.REFRESH_TS_DATA}
-        subTitle={t.REFRESH_TS_DATA_DESCRIPTION}
-        firstRadioButtonText={t.REFRESH_ALL_SHEETS}
-        secondRadioButtonText={t.REFRESH_CURRENT_SHEET}
-        onRadioSelectionChange={handleSelectionChange}
-        defaultRadioSelected={t.REFRESH_ALL_SHEETS}
-        firstButton={t.REFRESH_DATA}
-        firstButtonType={'SECONDARY'}
-        onFirstButtonClick={handleRefreshData}
-        isBottomBorderHidden={true}
-      />
+      {isPrivileged && (
+        <>
+          <Card
+            id={0}
+            title={t.INSERT_TS_DATA}
+            subTitle={t.INSERT_TS_DATA_DESCRIPTION}
+            firstButton={t.SELECT_TS_DATA}
+            firstButtonType={'PRIMARY'}
+            onFirstButtonClick={() => route(Routes.SEARCHBAR)}
+          />
+          <Card
+            id={1}
+            title={t.REFRESH_TS_DATA}
+            subTitle={t.REFRESH_TS_DATA_DESCRIPTION}
+            firstRadioButtonText={t.REFRESH_ALL_SHEETS}
+            secondRadioButtonText={t.REFRESH_CURRENT_SHEET}
+            onRadioSelectionChange={handleSelectionChange}
+            defaultRadioSelected={t.REFRESH_ALL_SHEETS}
+            firstButton={t.REFRESH_DATA}
+            firstButtonType={'SECONDARY'}
+            onFirstButtonClick={handleRefreshData}
+            isBottomBorderHidden={true}
+          />
+        </>
+      )}
     </Vertical>
   );
 };
