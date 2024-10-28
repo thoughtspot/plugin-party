e222ad4974011568b58bbd85a658965da4b9ad3a
Aditya Mittal <114516106+adityamittal3107@users.noreply.github.com>
2024-10-28 15:23:20 +0530

    Update home.tsx

--- packages/sheets/src/ui/components/home/home.tsx
+++ packages/sheets/src/ui/components/home/home.tsx
@@ -81,7 +81,7 @@ export const Home = () => {
       }
       setSuccess(true);
     } catch (error) {
-      if (error.message.includes('401')) {
+      if (error.message.includes('code 401')) {
         setSuccess(false);
         setErrorMessage({
           visible: true,
